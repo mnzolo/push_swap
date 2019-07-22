62288709fab9245e056ed2158c4dbdff4bb95e18
Mthokozisi NZOLO <mnzolo@c5r1s7.wethinkcode.co.za>
2019-07-22 12:07:38 +0200

    push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnzolo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 10:02:57 by mnzolo            #+#    #+#             */
+/*   Updated: 2019/07/22 09:27:30 by mnzolo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+
+typedef struct node
+{
+	int			data;
+	struct	node	*next;
+}				node;
+struct node 	*top;
+#endif
